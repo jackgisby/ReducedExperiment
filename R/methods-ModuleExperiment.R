190c475af024be3ce63b81193db2355c757d67e0
Jack Gisby <jackgisby@gmail.com>
2023-11-09 17:33:08 +0000

    Add basic module analysis and class methods

--- R/methods-ModuleExperiment.R
+++ R/methods-ModuleExperiment.R
@@ -1,6 +1,6 @@
 #' ModularExperiment
 #'
-#' A class to represent the results of factor analysis
+#' A class to represent the results of module analysis
 #'
 #' @export
 #' @importFrom SummarizedExperiment SummarizedExperiment
@@ -9,3 +9,103 @@ ModularExperiment <- function(assignments=character(), ...)
     re <- ReducedExperiment(...)
     return(.ModularExperiment(re, assignments=assignments))
 }
+
+S4Vectors::setValidity2("ModularExperiment", function(object) {
+    msg <- NULL
+
+    obj_dims <- dim(object)
+
+    # Features
+    if (obj_dims[3] != length(assignments(object)))
+        msg <- c(msg, "Assignments have invalid length")
+
+    if (!all.equal(assignments(object), rownames(object)))
+        msg <- c(msg, "Assignments have incompatible names (rownames)")
+
+    return(if (is.null(msg)) TRUE else msg)
+})
+
+setMethod("assignments", "ModularExperiment", function(x, as_list=FALSE) {
+    if (as_list)  {
+        a <- list()
+        for (comp in componentNames(x)) {
+            a[[comp]] <- assignments(x)[which(names(assignments(x)) == comp)]
+        }
+    } else {
+        a <- x@assignments
+    }
+
+    return(a)
+})
+
+setReplaceMethod("assignments", "ModularExperiment", function(x, value) {
+    x@assignments <- value
+    validObject(x)
+    return(x)
+})
+
+setMethod("moduleNames", "ModularExperiment", function(x, as_list=FALSE) {
+    return(assignments(x, as_list))
+})
+
+setReplaceMethod("moduleNames", "ModularExperiment", function(x, value) {
+    assignments(x) <- value
+    return(x)
+})
+
+setReplaceMethod("componentNames", "ModularExperiment", function(x, value) {
+    names(x@assignments) <- value
+    x <- callNextMethod(x, value)
+    validObject(x)
+    return(x)
+})
+
+setMethod("[", c("ModularExperiment", "ANY", "ANY", "ANY"),
+          function(x, i, j, k, ..., drop=FALSE)
+{
+    if (1L != length(drop) || (!missing(drop) && drop))
+        warning("'drop' ignored '[,", class(x), ",ANY,ANY-method'")
+
+    assignments <- x@assignments
+
+    if (!missing(k)) {
+        if (is.character(k)) {
+            fmt <- paste0("<", class(x), ">[k,] index out of bounds: %s")
+            k <- SummarizedExperiment:::.SummarizedExperiment.charbound(
+              k, componentnames(x), fmt
+            )
+      }
+
+        k <- as.vector(k)
+        assignments <- assignments[k, drop=FALSE]
+    }
+
+    out <- callNextMethod(x, i, j, k, ...)
+    BiocGenerics:::replaceSlots(out, assignments=assignments, check=FALSE)
+})
+
+setMethod("runEnrich", c("ModularExperiment"),
+          function(x, method="overrepresentation", feature_id_col="rownames", as_dataframe=FALSE, ...)
+{
+    if (method == "overrepresentation") {
+
+        modules <- assignments(x)
+
+        enrich_res <- reduced_oa(modules, ...)
+
+    } else {
+        stop("Enrichment method not recognised")
+    }
+
+    # for (comp in names(enrich_res)) {
+    #     if (!is.null(enrich_res[[comp]]@result)) {
+    #         if (nrow(enrich_res[[comp]]@result) >= 1) {
+    #           enrich_res[[comp]]@result$setting <- TRUE
+    #         }
+    #     }
+    # }
+
+    if (as_dataframe) enrich_res <- do.call("rbind", enrich_res)
+
+    return(enrich_res)
+})
--- R/modules.R
+++ R/modules.R
@@ -1,20 +1,70 @@
 #' Run WGCNA
 #' @export
-run_wgcna <- function(X, ...)
+identify_modules <- function(X, powers, min_r_squared = 0.85, max_mean_connectivity = 100,
+                             corType = "pearson", networkType = "signed", module_labels = "numbers",
+                             ...)
 {
     if (!inherits(X, "SummarizedExperiment")) {
-        X <- SummarizedExperiment(assays = list(normal = X))
+        X <- SummarizedExperiment(assays = list("normal" = X))
     }
 
-    wgcna_res <- run_wgcna(X, ...)
-    reduced_set <- ModularExperiment(X, reduced=wgcna_res$E,
-                                     assignments=wgcna_res$assignments)
+    wgcna_res <- run_wgcna(assay(X, "normal"), powers=powers, min_r_squared = min_r_squared,
+                           corType = corType, networkType=networkType, module_labels = module_labels,
+                           seed=seed, ...)
+    reduced_set <- ModularExperiment(X, reduced=wgcna_res$E, assignments=wgcna_res$assignments)
 
     return(reduced_set)
 }
 
-.se_to_me <- function(se, reduced, assignments) {
-    return(FactorisedExperiment(assignments=assignments, reduced=reduced,
-                                assays=assays(se), rowData=rowData(se),
-                                colData=colData(se), metadata=metadata(se)))
+.se_to_me <- function(se, assignments, loadings, reduced, center, scale) {
+    return(ModularExperiment(assignments=assignments, loadings=loadings,
+                             reduced=reduced,
+                             assays=assays(se), rowData=rowData(se),
+                             colData=colData(se), metadata=metadata(se)))
+}
+
+#' Run WGCNA for a data matrix
+#' @export
+run_wgcna <- function(X, seed=1, powers = 1:40,
+                      min_r_squared = 0.85, max_mean_connectivity = 100,
+                      corType = "pearson", networkType="signed",
+                      module_labels = "numbers", ...) {
+    set.seed(seed)
+
+    if (length(powers) > 1) {
+
+        if (corType == "pearson") {
+            corFnc <- WGCNA::cor
+        } else if (corType == "bicor") {
+            corFnc <- WGCNA::bicor
+        } else {
+            stop("`corType` must be one of 'pearson', 'bicor'")
+        }
+
+        threshold <- WGCNA::pickSoftThreshold(t(X), RsquaredCut = min_r_squared, powerVector = powers, corFnc = corFnc, networkType = networkType)
+
+        if (is.null(max_mean_connectivity)) {
+            power <- threshold$powerEstimate
+        } else {
+            power <- min(threshold$Power[threshold$SFT.R.sq > min_r_squared & threshold$mean.k. < max_mean_connectivity])
+        }
+
+    } else if (length(powers) == 1) {
+        power <- powers
+    } else {
+        stop("Powers must either be a single integer or a vector of integers to be tested")
+    }
+
+    bwms <- WGCNA::blockwiseModules(t(X), power=power, corType=corType, networkType=networkType, ...)
+
+    wgcna_res <- list(
+        assignments = bwms$colors,
+        E = bwms$MEs
+    )
+
+    # TODO: give option for using colours instead `module_labels`
+    names(wgcna_res$assignments) <- paste0("factor_", names(wgcna_res$assignments))
+    names(wgcna_res$E) <- paste0("factor_", names(wgcna_res$E))
+
+    return(wgcna_res)
 }
