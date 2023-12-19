fc56c5a91e83f0f3afd0b78be1044f3ae91d975c
Jack Gisby <jackgisby@gmail.com>
2023-12-19 15:03:51 +0000

    Move functions for creating classes with randomised data to auxiliary

--- R/auxiliary.R
+++ R/auxiliary.R
@@ -9,3 +9,33 @@
 
     return(m)
 }
+
+#' i (features), j (samples), k components)
+.createRandomisedReducedExperiment <- function(i, j, k) {
+    return(ReducedExperiment(
+        assays = list("normal"=.makeRandomData(i, j, "gene", "sample")),
+        reduced = .makeRandomData(j, k, "sample", "factor")
+    ))
+}
+
+#' i (features), j (samples), k components)
+.createRandomisedFactorisedExperiment <- function(i, j, k) {
+    return(FactorisedExperiment(
+        assays = list("normal"=.makeRandomData(i, j, "gene", "sample")),
+        reduced = .makeRandomData(j, k, "sample", "factor"),
+        loadings = .makeRandomData(i, k, "gene", "factor")
+    ))
+}
+
+#' i (features), j (samples), k components)
+.createRandomisedModularExperiment <- function(i, j, k) {
+
+    assignments <- paste0("gene_", 1:i)
+    names(assignments) <- paste0("module_", round(runif(i, 1, 4), 0))
+
+    return(ModularExperiment(
+        assays = list("normal"=.makeRandomData(i, j, "gene", "sample")),
+        reduced = .makeRandomData(j, k, "sample", "factor"),
+        assignments = assignments
+    ))
+}
