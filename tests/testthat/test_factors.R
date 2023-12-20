6a394b58b2719af5a9fc638eb3d25e2c7d9b10a0
Jack Gisby <jackgisby@gmail.com>
2023-12-20 19:29:02 +0000

    Add tests for factor functions

--- tests/testthat/test_factors.R
+++ tests/testthat/test_factors.R
@@ -2,30 +2,73 @@ context("reduce_data")
 
 test_that("Estimate factors", {
 
-    random_expr <- .makeRandomData(50, 30, "gene", "sample")
-    factor_exp <- estimate_factors(random_expr, method="ICA", nc=5, scale_X=FALSE)
+    # Use real data from airway package
+    data(airway, package="airway")
+    airway <- airway[apply(assay(airway, "counts"), 1, function(x) {all(x != 0)}) ,]
+    assay(airway, "normal") <- log(assay(airway, "counts") + 0.1)
 
-    expect_equal(dim(factor_exp), c("Features" = 50, "Samples" = 30, "Components" = 5))
+    airway_fe <- estimate_factors(airway, nc=2, seed=1)
 
-    reprojected_data <- projectData(factor_exp, random_expr)
-    expect_equal(reduced(factor_exp), reprojected_data)
-
-    factor_exp@center <- factor_exp@scale <- FALSE
-
-    reprojected_data <- projectData(factor_exp, t(scale(t(random_expr), scale=FALSE)))
-    expect_equal(reduced(factor_exp), reprojected_data)
-
-    # projectData(factor_exp, .makeRandomData(50, 20, "gene", "sample"))
+    expect_equal(dim(airway_fe), c("Features" = 18870, "Samples" = 8, "Components" = 2))
+    expect_equal(reduced(airway_fe)[1,], c("factor_1" = -0.001749838, "factor_2" = -0.27071251), tolerance=1e-5)
+    expect_equal(loadings(airway_fe)[1,], c("factor_1" = -1.2403327, "factor_2" = -0.2849629), tolerance=1e-5)
+    expect_equivalent(length(airway_fe@center), nFeatures(airway_fe))
 })
 
 test_that("run_ica matches estimate_factors", {
+
+    # Use real data from airway package
+    data(airway, package="airway")
+    airway <- airway[apply(assay(airway, "counts"), 1, function(x) {all(x != 0)}) ,]
+    assay(airway, "normal") <- log(assay(airway, "counts") + 0.1)
+
+    airway_fe <- estimate_factors(airway, nc=2, seed=1)
+    airway_ica <- run_ica(assay(airway, "normal"), nc=2, seed=1)
+
+    expect_equal(reduced(airway_fe), airway_ica$M, tolerance=1e-5)
+    expect_equal(loadings(airway_fe), airway_ica$S, tolerance=1e-5)
 })
 
 test_that("Stability ICA", {
 
+    # Use real data from airway package
+    data(airway, package="airway")
+    airway <- airway[apply(assay(airway, "counts"), 1, function(x) {all(x != 0)}) ,]
+    set.seed(2)
+    airway <- airway[sample(nrow(airway), 500) ,]
+    assay(airway, "normal") <- log(assay(airway, "counts") + 0.1)
+
+    airway_fe <- estimate_factors(airway, nc=2, seed=1, use_stability=TRUE, n_runs=5)
+
+    expect_equal(reduced(airway_fe)[1,], c("factor_1" = -0.2398201, "factor_2" = -0.0193603), tolerance=1e-5)
+    expect_equal(loadings(airway_fe)[1,], c("factor_1" = -0.01766713, "factor_2" = -1.18106665), tolerance=1e-5)
+    expect_equal(names(stability(airway_fe)), componentNames(airway_fe))
+    expect_true(all(stability(airway_fe) > 0.99))
+
+    airway_fe_bootstrap <- estimate_factors(airway, nc=2, seed=1, use_stability=TRUE, n_runs=5, resample=TRUE)
+
+    expect_equal(reduced(airway_fe_bootstrap)[1,], c("factor_1" = -0.06823572, "factor_2" = -0.27932475), tolerance=1e-5)
+    expect_equal(loadings(airway_fe_bootstrap)[1,], c("factor_1" = -0.7362659, "factor_2" = 0.1071863), tolerance=1e-5)
+    expect_equal(names(stability(airway_fe_bootstrap)), componentNames(airway_fe_bootstrap))
+    expect_true(all(stability(airway_fe_bootstrap) > 0.2))
 })
 
 test_that("Estimate stability", {
 
+    # Use real data from airway package
+    data(airway, package="airway")
+    airway <- airway[apply(assay(airway, "counts"), 1, function(x) {all(x != 0)}) ,]
+    set.seed(2)
+    airway <- airway[sample(nrow(airway), 500) ,]
+    assay(airway, "normal") <- log(assay(airway, "counts") + 0.1)
+
+    stability_res <- estimate_stability(airway, seed=1, n_runs=5, min_components=2, max_components=4, by=1, mean_stability_threshold=0.9)
+    stability_res_bootstrap <- estimate_stability(airway, seed=1, n_runs=5, min_components=2, max_components=4, by=1, mean_stability_threshold=0.9, resample=TRUE)
+
+    expect_equal(stability_res$selected_nc, 4)
+    expect_equal(stability_res_bootstrap$selected_nc, NULL)
+
+    expect_equal(colnames(stability_res$stability), c("nc", "component_name", "component_number", "stability"))
+    expect_equal(colnames(stability_res_bootstrap$stability), colnames(stability_res$stability))
 })
 
